2cf57eff2f873f17a4dfdc46c17205f27c986696
nexarentalsmallorca-jpg <nexarentalsmallorca@gmail.com>
2026-02-14 13:37:49 +0100

    hotfix: make time page build-safe

--- app/time/page.tsx
+++ app/time/page.tsx
@@ -1,6 +1,8 @@
+export const dynamic = "force-dynamic";
+
 export default function TimePage() {
   return (
-    <div style={{ padding: 40, color: "white", background: "black" }}>
+    <div style={{ padding: 40, background: "black", color: "white" }}>
       Time selection coming soon.
     </div>
   );
