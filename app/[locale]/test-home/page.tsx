dba42962ab7eaf6e6624aaf6ab9350a150099f58
nexarentalsmallorca-jpg <nexarentalsmallorca@gmail.com>
2026-04-14 10:32:11 +0200

    Fix suspense error

--- app/[locale]/test-home/page.tsx
+++ app/[locale]/test-home/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import HomeClientV2 from "../../HomeClientV2";
 
 export default function TestHomePage() {
-  return <HomeClientV2 />;
+  return (
+    <Suspense fallback={<div className="min-h-screen bg-[#0f1115]" />}>
+      <HomeClientV2 />
+    </Suspense>
+  );
 }
