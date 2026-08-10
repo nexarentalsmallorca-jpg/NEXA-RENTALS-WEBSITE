7d218f862d3d3d79dcf6ffa8447866e23a003d65
nexarentalsmallorca-jpg <nexarentalsmallorca@gmail.com>
2026-08-10 13:46:25 +0200

    Fix checkout document auto-fill

--- app/[locale]/checkout/CheckoutClient.tsx
+++ app/[locale]/checkout/CheckoutClient.tsx
@@ -1367,7 +1367,12 @@ export default function CheckoutClient() {
         ?.lastName ||
       "";
 
-    
+    const extractedAddress =
+      payload.identityData
+        ?.address ||
+      payload.licenceData
+        ?.address ||
+      "";
 
     if (
       extractedFirstName
